75e281f7d3fc1a91c480aa8b1be82aa17f144a21
Kiri4n <kirian.jourand@gmail.com>
2022-06-19 11:46:48 +0200

    🗃️ add data to armures table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('armures')->insert([
+            'materiau' => 'Tissu'
+        ]);
+        DB::table('armures')->insert([
+            'materiau' => 'Cuire'
+        ]);
+        DB::table('armures')->insert([
+            'materiau' => 'Métal'
+        ]);
     }
 }
