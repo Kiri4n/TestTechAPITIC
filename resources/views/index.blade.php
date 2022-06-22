d7bfa95e684e37db3f353333d09370bfb6b99e4a
Kiri4n <kirian.jourand@gmail.com>
2022-06-22 10:43:58 +0200

    ✨ indexController, vue, route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <title>TestTechAPITIC</title>
+</head>
+<body>
+    <h1>YO</h1>
+    <?php
+        dd($materiauxArmure);
+    ?>
+</body>
