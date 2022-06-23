28fb44dd8698db83b9c12d65b915c295807a62f5
Kiri4n <kirian.jourand@gmail.com>
2022-06-23 14:09:18 +0200

    ✨ add relation One to One

--- app/Models/personnage.php
+++ app/Models/personnage.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class personnage extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'personnage_id';
+
+    public function armure(){
+        return $this->hasOne(armure::class, 'armure_id', 'armure_id');
+    }
+
+    public function race(){
+        return $this->hasOne(race::class, 'race_id', 'race_id');
+    }
+
+    public function classe(){
+        return $this->hasOne(classe::class, 'classe_id', 'classe_id');
+    }
+
+    public function specialisation(){
+        return $this->hasOne(specialisation::class, 'specialisation_id', 'specialisation_id');
+    }
 }
