551b7b1d984bb248d3042e83bfc5203f21e39523
Kiri4n <kirian.jourand@gmail.com>
2022-06-23 10:42:32 +0200

    💄 structure UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,45 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="/css/app.css">
-    <title>TestTechAPITIC</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+
+
+    <title>Test Tech - Kirian JOURAND</title>
 </head>
 <body>
-    <h1>YO</h1>
-    <?php
-        dd($materiauxArmure);
-    ?>
+<div class="container mt-4">
+    <h1>Test Technique APITIC - Kirian JOURAND</h1>
+    <button type="button" class="btn btn-primary mt-5">Ajouter</button>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Pseudo</th>
+            <th>Race</th>
+            <th>Point de vie</th>
+            <th>Armure</th>
+            <th>Détail</th>
+            <th>Propriétaire</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <?php
+
+            ?>
+        </tr>
+        </tbody>
+    </table>
+</div>
+    <script src="{{asset('js/app.css')}}"></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
 </body>
+</html>
